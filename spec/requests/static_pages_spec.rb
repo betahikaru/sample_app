07cb8cf9e68c3ff65e2d8ce308793de46c535f7f
betahikaru <dev.beta.hikaru2000@gmail.com>
2013-07-13 00:35:16 +0900

    add test for 'About Us'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,6 +34,11 @@ describe "Static pages" do
 
     it "should have the h1 'About Us'" do 
       visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
       page.should have_selector('title',
         :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
